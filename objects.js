f2db3a2970374882f9774c075624afa7c4d807b2
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-29 19:23:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,9 @@
 var playlist = {
-  artist: 'ArtistValue',
-  song: 'songValue'
+  artist1: 'song1'
+}
+function updatePlaylist(playlist,artistName,songTitle){
+  playlist.artistName = 'songTitle'
+}
+function removeFromPlaylist(playlist,artistName){
+delete playlist.artistName  
 }
